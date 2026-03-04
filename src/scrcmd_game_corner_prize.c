f720c616b469c4bd4286655dc7175e37b74cc74a
dlzwz <da.lobato95@gmail.com>
2026-03-03 23:36:13 -0500

    Reduce Game Corner prize costs by 100x

--- src/scrcmd_game_corner_prize.c
+++ src/scrcmd_game_corner_prize.c
@@ -20,25 +20,25 @@ BOOL ScrCmd_GetGameCornerPrizeData(ScriptContext *ctx)
     u16 *price = ScriptContext_GetVarPointer(ctx);
 
     static const GameCornerPrize sGameCornerPrizeData[] = {
-        { ITEM_SILK_SCARF, 1000 },
-        { ITEM_WIDE_LENS, 1000 },
-        { ITEM_ZOOM_LENS, 1000 },
-        { ITEM_METRONOME, 1000 },
-        { ITEM_TM90, 2000 },
-        { ITEM_TM58, 2000 },
-        { ITEM_TM75, 4000 },
-        { ITEM_TM32, 4000 },
-        { ITEM_TM44, 6000 },
-        { ITEM_TM89, 6000 },
-        { ITEM_TM10, 6000 },
-        { ITEM_TM27, 8000 },
-        { ITEM_TM21, 8000 },
-        { ITEM_TM35, 10000 },
-        { ITEM_TM24, 10000 },
-        { ITEM_TM13, 10000 },
-        { ITEM_TM29, 10000 },
-        { ITEM_TM74, 15000 },
-        { ITEM_TM68, 20000 }
+        { ITEM_SILK_SCARF, 10 },
+        { ITEM_WIDE_LENS, 10 },
+        { ITEM_ZOOM_LENS, 10 },
+        { ITEM_METRONOME, 10 },
+        { ITEM_TM90, 20 },
+        { ITEM_TM58, 20 },
+        { ITEM_TM75, 40 },
+        { ITEM_TM32, 40 },
+        { ITEM_TM44, 60 },
+        { ITEM_TM89, 60 },
+        { ITEM_TM10, 60 },
+        { ITEM_TM27, 80 },
+        { ITEM_TM21, 80 },
+        { ITEM_TM35, 100 },
+        { ITEM_TM24, 100 },
+        { ITEM_TM13, 100 },
+        { ITEM_TM29, 100 },
+        { ITEM_TM74, 150 },
+        { ITEM_TM68, 200 }
     };
 
     *item = sGameCornerPrizeData[index].item;
